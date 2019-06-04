16eb75083385520a0720060d5691df25cf15dfc1
Shima <shima.jin@icloud.com>
2019-06-04 16:41:21 -0700

    Initial Commit

---
+++ swiftui-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftui-practice
+//
+//  Created by jinsei_shima on 2019/06/04.
+//  Copyright © 2019 jinsei_shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
